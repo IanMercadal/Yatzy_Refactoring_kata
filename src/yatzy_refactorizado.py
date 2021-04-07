15ea0cbd455d0203e384d775773e7d6750fe6659
Ian Mercadal <imercadal@cifpfbmoll.eu>
2021-04-07 16:39:59 +0200

    refactor: :art:

--- src/yatzy_refactorizado.py
+++ src/yatzy_refactorizado.py
@@ -44,67 +44,44 @@ class Yatzy:
         return self.dice.count(six) * six
     
     @staticmethod
-    def score_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        at = 0
-        for at in range(6):
-            if (counts[6-at-1] == 2):
-                return (6-at)*2
-        return 0
+    def pair(*dice):
+        pair = 2
+        for numero in range (6, 0, -1):
+            if dice.count(numero) >= pair:
+                return pair * numero
+            return 0
     
     @staticmethod
-    def two_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        n = 0
-        score = 0
-        for i in range(6):
-            if (counts[6-i-1] >= 2):
-                n = n+1
-                score += (6-i)
-                    
-        if (n == 2):
-            return score * 2
+    def two_pairs(*dice):
+        pair = 2
+        pairs = 0
+        total = 0
+        numero = 1
+        while pairs < 2 and numero <= 6:
+            if dice.count(numero) >= 2:
+                pairs += 1
+                total += pair * numero
+            numero += 1
+        if pairs == 2:
+            return total
         else:
             return 0
     
     @staticmethod
-    def four_of_a_kind( _1,  _2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[_1-1] += 1
-        tallies[_2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        for i in range(6):
-            if (tallies[i] >= 4):
-                return (i+1) * 4
-        return 0
-    
-
+    def three_of_a_kind(*dice):
+        three = 3
+        for numero in range (6, 0, -1):
+            if dice.count(numero) >= three:
+                return three * numero
+            return 0
     @staticmethod
-    def three_of_a_kind( d1,  d2,  d3,  d4,  d5):
-        t = [0]*6
-        t[d1-1] += 1
-        t[d2-1] += 1
-        t[d3-1] += 1
-        t[d4-1] += 1
-        t[d5-1] += 1
-        for i in range(6):
-            if (t[i] >= 3):
-                return (i+1) * 3
-        return 0
-    
-
+    def four_of_a_kind(*dice):
+        four = 4
+        for numero in range (6, 0, -1):
+            if dice.count(numero) >= four:
+                return four * numero
+            return 0
+        
     @staticmethod
     def smallStraight( d1,  d2,  d3,  d4,  d5):
         tallies = [0]*6
