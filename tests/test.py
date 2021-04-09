75799d7fb51d63aab83cce1d6edd1abe12cfc63d
Ian Mercadal <imercadal@cifpfbmoll.eu>
2021-04-09 10:58:12 +0200

    test:

--- tests/test.py
+++ tests/test.py
@@ -3,3 +3,7 @@ from src.yatzy_refactorizado import Yatzy
 # These unit tests can be run using the py.test framework
 # available from http://pytest.org/
 
+def test_chance():
+        assert 5 ==  Yatzy.chance(1,1,1,1,1) 
+        assert 15 == Yatzy.chance(2,3,4,5,1)
+        assert 16 == Yatzy.chance(3,3,4,5,1)
